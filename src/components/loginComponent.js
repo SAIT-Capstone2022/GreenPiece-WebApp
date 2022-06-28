e3c188cfde68ca588324438bed49f6235db68650
David d'Entremont <90799994+DaviddEntremont@users.noreply.github.com>
2022-06-28 13:24:39 -0600

    Test

--- src/components/loginComponent.js
+++ src/components/loginComponent.js
@@ -1,6 +1,65 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Login extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    
+        this.state = {
+          email: '',
+          password: '',
+          users: []
+        }
+      }
+
+      onChangeEmail(e) {
+        this.setState({
+          email: e.target.value
+        })
+      }
+    
+      onChangePassword(e) {
+        this.setState({
+          password: e.target.value
+        })
+      }
+
+      onSubmit(e) {
+        e.preventDefault();
+    
+        axios.get('http://localhost:5000/users/get')
+          .then(res => {console.log(res.data) 
+            this.setState({
+              email: '',
+              password: '',
+              username: '',
+              phonenumber: '',
+              message: "Account successfully created."
+            })
+          })
+          .catch(err => {
+            this.setState({
+              email: this.state.email,
+              password: this.state.password,
+              username: this.state.username,
+              phonenumber: this.state.phonenumber,
+              message: "Email already in use, please try a different one."
+            })
+          })
+
+          const user = {
+            email: this.state.email,
+            password: this.state.password,
+          }
+      
+          console.log(user);
+      }
+
+
     render() {
         return (
             <div class="container p-4 m-2 mx-auto mt-3 bg-light rounded shadow-lg">
