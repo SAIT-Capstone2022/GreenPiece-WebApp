a51d056e5dd6bd0af10a5c9ebd02a3c44b7c2382
ReiEmRo <reiemanuelr@gmail.com>
2022-07-05 16:13:02 -0600

    minor changes to iframe.

--- src/components/dashboardComponent.js
+++ src/components/dashboardComponent.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
 
-const chart = <iframe width="1200" height="750" src="https://charts.mongodb.com/charts-greenhouse-project-zfldl/embed/dashboards?id=62bb709e-5246-46c9-801d-18966de071e1&theme=light&autoRefresh=true&maxDataAge=60&showTitleAndDesc=false&scalingWidth=fixed&scalingHeight=fixed"></iframe>;
+const chart = <iframe title="Dashboard Graph" width="1200" height="750" src="https://charts.mongodb.com/charts-greenhouse-project-zfldl/embed/dashboards?id=62bb709e-5246-46c9-801d-18966de071e1&theme=light&autoRefresh=true&maxDataAge=60&showTitleAndDesc=false&scalingWidth=fixed&scalingHeight=fixed"></iframe>;
 
 export default class DashboardComponent extends Component {
   render() {
-    const getUsername = () => {
-      const data = localStorage.getItem("token");
-
-      return data.email;
-    }
-
     return (
       <div className="container p-4 m-2 mx-auto mt-3 bg-light rounded shadow-lg">
         <h2>Name's Dashboard</h2>
