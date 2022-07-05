cfb590e256a2445737ae0f83d5fae7dd6864a391
Jin-young Park <pjy@gmail.com>
2022-07-05 13:11:16 -0600

    deleted maincomponentjs and unneeded codes

--- src/components/dashboardComponent.js
+++ src/components/dashboardComponent.js
@@ -23,22 +23,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-
-
-// import React, { Component } from 'react';
-
-// export default class DashboardComponent extends Component {
-//   render() {
-//     return (
-//       <div className="container p-4 m-2 mx-auto mt-3 bg-light rounded shadow-lg">
-//         <h2>Name's Dashboard</h2>
-//         <div class="my-3">
-//           <div id="graph-container">
-            
-//           </div>
-//         </div>
-//       </div>
-//     );    
-//   }
-// }
-
