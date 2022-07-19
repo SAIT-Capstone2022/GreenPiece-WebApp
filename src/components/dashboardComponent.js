f142292dcc66516cfb469e8ddad10430aaca5374
Jin-young Park <pjy@gmail.com>
2022-07-19 13:51:54 -0600

    Edited dashboardComponent

--- src/components/dashboardComponent.js
+++ src/components/dashboardComponent.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Card, Row, Alert } from 'react-bootstrap';
+import axios from "axios";
 
 const chart = <iframe title="Dashboard Graph" width="800" height="500" src="https://charts.mongodb.com/charts-greenhouse-project-zfldl/embed/dashboards?id=62bb709e-5246-46c9-801d-18966de071e1&theme=light&autoRefresh=true&maxDataAge=60&showTitleAndDesc=false&scalingWidth=fixed&scalingHeight=fixed"></iframe>;
 
-// const userObject = JSON.parse(localStorage.getItem("user"));
-// const user = userObject.username;
+const DashboardComponent = () => {
 
-const token = localStorage.getItem("token");
+const thisUser = localStorage.getItem("user");
 
-console.log(token);
+const userObject = JSON.parse(thisUser);
 
-export default class DashboardComponent extends Component {
-  render() {
+const username = userObject.username;
+
+//const [data, setData] = useState({email: userObject.email});
+
+//setData({ ...data, ["email"]: userObject.email });
+
+/*const getChartData = async (e) => {
+  e.preventDefault();
+  const url = "http://localhost:5000/data/getSensorData";
+  const { data: res } = await axios.post(url, data);
+  console.log(res);
+  const [temperatureData, settemperatureData] = useState({ labels: "imgay" });
+  };*/
+
+console.log(userObject.email);
+
     return (
+      <div /*onLoad={getChartData}*/ className="container p-4 m-3 mx-auto bg-light rounded shadow-lg">
+        <h2> {username} 's Dashboard</h2>
 
-      <div className="container p-4 m-3 mx-auto bg-light rounded shadow-lg">
-        {/* <h2> {user}'s Dashboard</h2> */}
-        <h2> 's Dashboard</h2>
         <div className="my-3">
           <Row className='my-3 p-3'>
-            <div id="chart-container" className="mx-auto col-8">
+            <div id="chart-container" className="mx-auto col-lg-8">
               {chart}
             </div>
-            <div className='col-4 rounded shadow-lg'>
+            <div className='col-lg-4 rounded shadow-lg'>
               <Card.Body className='p-3'>
                 <Card.Title>Weather Header</Card.Title>
                 <hr />
@@ -39,6 +52,6 @@ export default class DashboardComponent extends Component {
         </div>
       </div>
     );
-  }
-}
+  };
 
+  export default DashboardComponent;
