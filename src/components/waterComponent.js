b6252b1e15f9a729f4d52b38d42a839eff82b628
ReiEmRo <reiemanuelr@gmail.com>
2022-07-21 11:04:41 -0600

    Added more content to login page. Modifications to water component.

--- src/components/waterComponent.js
+++ src/components/waterComponent.js
@@ -1,61 +1,60 @@
 import { useState } from "react";
+import { Navigate } from 'react-router-dom';
+import Calendar from 'react-calendar';
+import './calendar.css';
 import axios from "axios";
 import React from "react";
 import { Spinner, Alert } from 'react-bootstrap';
 
-
-
 const Water = () => {
-
 	const [msg, setMsg] = useState("");
 	const [showMsg, setShowMsg] = useState(true);
 	const [isLoading, setLoading] = useState(false);
+	const [date, dateOnChange] = useState(new Date());
+	const weekStart = 3;
 
 	const user = localStorage.getItem("user");
 	const userobject = JSON.parse(user);
 
+	if (userobject == null) {
+		return (
+			<Navigate to="/login" replace={true} />
+		);
+	}
+
 	const [data, setData] = useState({
-		water: userobject.username
-	  });
+		_id: userobject._id
+	});
 
-	const handleChange = ({ currentTarget: input }) => {
-		setData({ ...data, [input.name]: input.value });
-	};
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setMsg("");
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setMsg("");
 		setLoading(true);
-        try {
-            console.log("aaa");
-            const url = "http://localhost:5000/water/water";
-            console.log("err");
+		try {
+			const url = "http://localhost:5000/users/water-update";
 			const { data: res } = await axios.post(url, data);
-            setMsg(res.message);
+			setMsg(res.message);
 			setShowMsg(true);
 			setLoading(false);
 			setData({
 				water: ""
 			});
-        } catch (error) {
-            console.log(error);
-        }
-    }
-    return (
-        <div class="container p-4 m-2 mx-auto mt-3 bg-light rounded shadow-lg" id="signup-component">
-            <h3 className="mb-4">Water</h3>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group py-2">
-					<h5>Water</h5>
-					<input
-						type="hidden"
-						name="water"
-						onChange={handleChange}
-						value={userobject.username}
-						required
-						className="form-control"
-					/>
-                </div>
+		} catch (error) {
+			setLoading(false);
+			console.log(error);
+		}
+	}
+	return (
+		<div className="container p-4 mt-3 mb-5 mx-auto bg-light rounded shadow-lg" id="signup-component">
+			<h3 className="mb-4">Watering History</h3>
+			<form onSubmit={handleSubmit}>
+				<div className="pb-4">
+					<Calendar className="mx-auto rounded" onChange={dateOnChange} value={date} calendarType="US" />
+				</div>
+
+				<div className="form-group py-2">
+					<h5 className="m-0">Have you watered your plants today?</h5>
+				</div>
 
 				{msg &&
 					<Alert show={showMsg} variant="success" className="mt-2">
@@ -63,15 +62,15 @@ const Water = () => {
 					</Alert>
 				}
 
-				<div className="form-group py-2 mt-2">
-					<input type="submit" value="Water" className="btn btn-primary" />
+				<div className="form-group mt-2">
+					<input type="submit" value="Watered" className="btn btn-success" />
 					<span className="m-2 p-2">
 						{isLoading ? <Spinner animation="border" size="sm" variant="secondary" /> : ""}
 					</span>
 				</div>
-            </form>
-        </div>
-    );
+			</form>
+		</div>
+	);
 };
 
 export default Water;
