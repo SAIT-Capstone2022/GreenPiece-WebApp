c91ad836ffc2ec73a08c85bf98e092e61ed84db0
Jin-young Park <pjy@gmail.com>
2022-08-03 23:10:57 -0600

    Added mygreenhouse on the navbar.

--- src/components/navbarComponent.js
+++ src/components/navbarComponent.js
@@ -28,6 +28,9 @@ export default class Navbar extends Component {
                             <li className="nav-item py-2">
                                 <Link to="/water" className="nav-link" href="water.html">Water</Link>
                             </li>
+                            <li className="nav-item py-2">
+                                <Link to="/mygreenhouse" className="nav-link" href="mygreenhouse.html">My Greenhouse</Link>
+                            </li>
                             {/* <li className="nav-item">
                                 <Link to="/settings" className="nav-link" href="settings.html">Settings</Link>
                             </li> */}
